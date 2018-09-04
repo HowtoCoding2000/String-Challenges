3f5a801051606bc870824ef7f1bd81b71ed2194e
calvin.farrell <calvin.farrell@MU-C1-12.mullen.pvt.k12.co.us>
2018-09-04 14:54:46 -0600

    finished string challenges

--- Strings Challenges/src/StringChallenges.java
+++ Strings Challenges/src/StringChallenges.java
@@ -1,50 +1,131 @@
 import java.util.Scanner;
 public class StringChallenges
 	{
+		static boolean stillRunningCode = true;
 		static Scanner userInput = new Scanner(System.in);
+		static Scanner userStringput = new Scanner(System.in);
+		static boolean stillAsking = true;
 		static boolean stillPlaying = true;	
 		static String shout = "";
 		static int codeSelection;
 		static String text = "";
 		public static void main(String[] args)
 			{
-				askWhichCodeToRun();
-				runSelectedCode();
+				while (stillRunningCode)
+					{
+						askWhichCodeToRun();
+						runSelectedCode();
+						askToChangeCodes();
+					}
 			}
 		public static void askWhichCodeToRun()
 			{
-				System.out.println("Would you like to run (1) UseYourInsideVoice or (2) VerticalLetters");
+				System.out.println("Would you like to run (1) UseYourInsideVoice, (2) VerticalLetters, or (3) CountTheVowels?");
 				codeSelection = userInput.nextInt();
 			}
 		public static void runSelectedCode()
 			{
-				if (codeSelection == 1)
+				stillAsking = true;
+				while (stillAsking)
 					{
-						useYourInsideVoice();
+						if (codeSelection == 1)
+							{
+								useYourInsideVoice();
+								stillAsking = false;
+							}
+						else if (codeSelection == 2)
+							{
+								verticalLetters();
+								stillAsking = false;
+							}
+						else if (codeSelection == 3)
+							{
+								countTheVowels();
+								stillAsking = false;
+							}
+						else
+							{
+								System.out.println("Please choose (1) UseYourInsideVoice, (2) VerticalLetters, or (3) CountTheVowels.");
+								codeSelection = userInput.nextInt();
+							}
 					}
-				else
+			}
+		public static void askToChangeCodes()
+			{
+				stillAsking = true;
+				while (stillAsking)
 					{
-						verticalLetters();
+						System.out.println("Would you like to run another code?");
+						System.out.println("(1) Yes or (2) No");
+						int runAnotherCode = userInput.nextInt();
+						if (runAnotherCode == 1)
+							{
+								stillAsking = false;
+							}
+						else if (runAnotherCode == 2)
+							{
+								System.out.println("Thank you for running StingChallenges.");
+								stillRunningCode = false;
+								stillAsking = false;
+							}
+						else
+							{
+								stillAsking = true;
+							}
 					}
+				
 			}
 		public static void useYourInsideVoice()
 			{
+				stillPlaying = true;
 				while (stillPlaying)
 					{						
-						askForYellInput();
-						convertToLowerCase();
+						System.out.println("Yell something.");
+						shout = userStringput.nextLine();
+						shout = shout.substring(0 , 1) + shout.substring(1 , shout.length()).toLowerCase();
+						System.out.println(shout);
 						askToPlayAgain();
 					}
 			}
-		public static void askForYellInput()
+		public static void verticalLetters()
 			{
-				System.out.println("Yell something.");
-				shout = userInput.nextLine();
+				stillPlaying = true;
+				while (stillPlaying)
+					{
+						System.out.println("Type something.");
+						text = userStringput.nextLine();
+						for (int i = 0; i < text.length(); i ++)
+							{
+								System.out.println(text.substring(i, i + 1));
+							}
+						askToPlayAgain();
+					}
 			}
-		public static void convertToLowerCase()
+		public static void countTheVowels()
 			{
-				shout = shout.substring(0 , 1) + shout.substring(1 , shout.length()).toLowerCase();
-				System.out.println(shout);
+				stillPlaying = true;
+				while (stillPlaying)
+					{
+						System.out.println("Type something.");
+						text = userStringput.nextLine();
+						int numberOfVowels = 0;
+						for (int i = 0; i < text.length(); i++)
+							{
+								switch (text.substring(i, i + 1))
+									{
+									case "a":
+									case "e":
+									case "i":
+									case "o":
+									case "u":
+											{
+												numberOfVowels = numberOfVowels + 1;
+											}
+									}
+							}
+						System.out.println("There are " + numberOfVowels + " vowels in that text.");
+						askToPlayAgain();
+					}
 			}
 		public static void askToPlayAgain()
 			{
@@ -60,24 +141,6 @@ public class StringChallenges
 				else
 					{
 						stillPlaying = true;
-					}
-			}
-		public static void verticalLetters()
-			{
-				askForInput();
-				printTextVertically();
-				askToPlayAgain();
-			}
-		public static void askForInput()
-			{
-				System.out.println("Type something.");
-				text = userInput.nextLine();
-			}
-		public static void printTextVertically()
-			{
-				for (int i = 0; i < text.length(); i ++)
-					{
-						System.out.println(text.substring(i, i + 1));
 					}
 			}
 	}
